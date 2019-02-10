881633e241c325a93619b8e7478c33bd09162830
Nickita Taranov <nickita.taranov@gmail.com>
2019-02-10 14:33:53 +0300

    initial commit

---
+++ constructible_from/constructible_from.hpp
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <type_traits>
+
+namespace detail {
+
+template <class Data, class T, class... Args>
+struct ConstructibleFrom {
+  constexpr ConstructibleFrom(Args... args) noexcept(
+      std::is_nothrow_constructible_v<T, Args...>) {
+    // A bit of hackery. The whole Data object is not constructed
+    // yet and thus accessing its member is not a good idea (precisely, UB). But
+    // |mem| is not an own member of Data, it is derived from Holder which is
+    // perfectly constructed to that moment, since Holder is another base of
+    // Data, preceding ConstructibleFrom in the base-specifier-list.
+    auto dataPtr = static_cast<Data*>(this);
+    new (&dataPtr->mem) T(std::forward<Args>(args)...);
+  }
+};
+
+}  // namespace detail
+
+template <class DataType, class... ConstructionArgs>
+class ConstructibleFrom {
+  using AlignedStorage =
+      std::aligned_storage_t<sizeof(DataType), alignof(DataType)>;
+
+ public:
+  struct Type;
+
+  using ConstructionTrait =
+      detail::ConstructibleFrom<Type, DataType, ConstructionArgs...>;
+
+  struct Holder {
+    AlignedStorage mem;
+  };
+
+  // The constructor of ConstructionTrait became the only possible constructor
+  // for Type, since it have no own constructors.
+  // Also note that detail::ConstructibleFrom is not an aggregate type, so it
+  // can be initialized only through constructor call.
+  struct Type : Holder, ConstructionTrait {
+    using ConstructionTrait::ConstructionTrait;
+
+    operator DataType() {
+      DataType* data = reinterpret_cast<DataType*>(&this->mem);
+      return *data;
+    }
+  };
+};
